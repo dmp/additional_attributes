b433472923cc9e1e7c987ce4d09f252b7a8830f4
dmp <dmp@users.noreply.github.com>
2025-05-06 14:09:46 +0900

    Add import to be compiled with Java 17
    /src/main/java/de/cadentem/additional_attributes/compat/apotheosis/AffixUtils.java:24: error: cannot find symbol

--- src/main/java/de/cadentem/additional_attributes/compat/apotheosis/AffixUtils.java
+++ src/main/java/de/cadentem/additional_attributes/compat/apotheosis/AffixUtils.java
@@ -14,6 +14,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 
 import java.util.List;
+import java.util.Objects;
 
 public class AffixUtils {
     public static void affixItem(final ItemStack stack, final Player player) {
